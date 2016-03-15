336f16f567a1adb3344dee5ab5be956eab6a1bb7
Bartosz Nowak <sztosz@gmail.com>
2016-03-15 21:30:04 +0100

    Fix wrong column type in Servicemen migration

--- db/migrate/20160315194827_create_servicemen.rb
+++ db/migrate/20160315194827_create_servicemen.rb
@@ -4,7 +4,7 @@ class CreateServicemen < ActiveRecord::Migration
       t.string :name
       t.integer :phone
       t.string :email
-      t.reference :service, index: true, foreign_key: true, null: false
+      t.belongs_to :service, index: true, foreign_key: true, null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160315193230) do
+ActiveRecord::Schema.define(version: 20160315200709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,9 +23,32 @@ ActiveRecord::Schema.define(version: 20160315193230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "client_id"
+    t.integer  "service_id"
   end
 
   add_index "addresses", ["client_id"], name: "index_addresses_on_client_id", using: :btree
+  add_index "addresses", ["service_id"], name: "index_addresses_on_service_id", using: :btree
+
+  create_table "claims", force: :cascade do |t|
+    t.integer  "operator_id"
+    t.integer  "client_id"
+    t.integer  "service_id"
+    t.integer  "location_id"
+    t.boolean  "valid",         null: false
+    t.integer  "serviceman_id"
+    t.boolean  "closed",        null: false
+    t.integer  "status_id"
+    t.text     "description",   null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "claims", ["client_id"], name: "index_claims_on_client_id", using: :btree
+  add_index "claims", ["location_id"], name: "index_claims_on_location_id", using: :btree
+  add_index "claims", ["operator_id"], name: "index_claims_on_operator_id", using: :btree
+  add_index "claims", ["service_id"], name: "index_claims_on_service_id", using: :btree
+  add_index "claims", ["serviceman_id"], name: "index_claims_on_serviceman_id", using: :btree
+  add_index "claims", ["status_id"], name: "index_claims_on_status_id", using: :btree
 
   create_table "clients", force: :cascade do |t|
     t.integer  "phone"
@@ -33,6 +56,15 @@ ActiveRecord::Schema.define(version: 20160315193230) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "locations", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +74,24 @@ ActiveRecord::Schema.define(version: 20160315193230) do
 
   create_table "operators", force: :cascade do |t|
     t.string   "name"
+    t.integer  "phone"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "servicemen", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "phone"
+    t.string   "email"
+    t.integer  "service_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "servicemen", ["service_id"], name: "index_servicemen_on_service_id", using: :btree
+
+  create_table "services", force: :cascade do |t|
     t.integer  "phone"
     t.string   "email"
     t.datetime "created_at", null: false
@@ -86,7 +136,26 @@ ActiveRecord::Schema.define(version: 20160315193230) do
   add_index "users", ["operator_id"], name: "index_users_on_operator_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  create_table "waybills", force: :cascade do |t|
+    t.integer  "transport_service_id", null: false
+    t.string   "number"
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
+  end
+
+  add_index "waybills", ["transport_service_id"], name: "index_waybills_on_transport_service_id", using: :btree
+
   add_foreign_key "addresses", "clients"
+  add_foreign_key "addresses", "services"
+  add_foreign_key "claims", "clients"
+  add_foreign_key "claims", "locations"
+  add_foreign_key "claims", "operators"
+  add_foreign_key "claims", "servicemen"
+  add_foreign_key "claims", "services"
+  add_foreign_key "claims", "statuses"
+  add_foreign_key "comments", "users"
+  add_foreign_key "servicemen", "services"
   add_foreign_key "users", "clients"
   add_foreign_key "users", "operators"
+  add_foreign_key "waybills", "transport_services"
 end
