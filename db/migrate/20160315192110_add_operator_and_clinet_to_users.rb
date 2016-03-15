aa3fc863e890a73f0ae93db8c8838c7f837ee3a2
Bartosz Nowak <sztosz@gmail.com>
2016-03-15 20:47:20 +0100

    Removed broken migration

--- db/migrate/20160315192110_add_operator_and_clinet_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddOperatorAndClientToUsers < ActiveRecord::Migration
-  def change
-    add_belongs_to :users, :operator, index: true, null: true
-    add_belongs_to :users, :operator, index: true, null: true
-  end
-end
