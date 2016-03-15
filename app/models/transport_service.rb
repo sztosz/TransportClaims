a681549b55cd71f182e79446dd49d257adf033ea
Bartosz Nowak <sztosz@gmail.com>
2016-03-15 19:47:15 +0100

    Fix TransportService validator

--- app/models/transport_service.rb
+++ app/models/transport_service.rb
@@ -1,5 +1,5 @@
 class TransportService < ActiveRecord::Base
   validates :phone, :email, :name, presence: true
-  validates :phone, numericality: { only_integer: true, greater_than: 0, less_than: 10 }
+  validates :phone, numericality: { only_integer: true, greater_than: 0, less_than: 999_999_999 }
   validates :phone, format: /^\d{9}$/
 end
