0ffe43817b477eeeb8f8b5b61368eb51700094b0
Bartosz Nowak <sztosz@gmail.com>
2016-03-15 20:34:25 +0100

    Add foreign_keys to migration

--- db/migrate/20160315192110_add_operator_and_client_to_users.rb
+++ db/migrate/20160315192110_add_operator_and_client_to_users.rb
@@ -1,6 +1,6 @@
 class AddOperatorAndClientToUsers < ActiveRecord::Migration
   def change
-    add_belongs_to :users, :operator, index: true, null: true
-    add_belongs_to :users, :client, index: true, null: true
+    add_belongs_to :users, :operator, index: true, foreign_key: true, null: true
+    add_belongs_to :users, :client, index: true, foreign_key: true, null: true
   end
 end
